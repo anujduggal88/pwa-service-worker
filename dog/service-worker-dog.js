b22ea820a896a4f7df2ebfdec05fc6a2b70bab33
Anuj Duggal <duggal@duggal-macbookpro.roam.corp.google.com>
2016-10-11 16:07:51 +0530

    Adding Multiple Service Workers
    Multiple Service Workers to demonstrate the usage and implementation
    
    Signed-off-by: Anuj Duggal <duggal@duggal-macbookpro.roam.corp.google.com>

--- dog/service-worker-dog.js
+++ dog/service-worker-dog.js
@@ -5,6 +5,23 @@ importScripts('../serviceworker-cache-polyfill.js');
 // INSTALL SERVICE WORKER:
 self.addEventListener('install', function(event){
 	console.info('[SUCCESS] service-worker-dog installed', + event);
+
+	event.waitUntil(
+
+		// Opens a new cache
+		caches.open('cache_sw_dog').then(function(cache){
+			console.info('[SUCCESS] Loading Cache');
+			return cache.addAll([
+
+				// Load this array into cache
+				'/',
+				'../style.css'
+				//new Request('https://farm6.staticflickr.com/5594/14749918329_888df4f2ef.jpg')
+			]);
+		}).catch(function(){
+			console.info('[FAILURE] Loading Cache');
+		})
+	);
 });
 
 // ACTIVATE SERVICE WORKER:
@@ -12,29 +29,19 @@ self.addEventListener('activate', function(event){
 	console.info('[SUCCESS] service-worker-dog activated', + event);
 });
 
+// FETCH EVENT:
 self.addEventListener('fetch', function(event){
 	console.info('[FETCH] ' + event.request.url);
-	
+
+	// Fetching from Cache:
 	event.respondWith(
-		// Fetch from Network:
-	    fetch(event.request).then(function(response){
+	    caches.match(event.request).then(function(response){
 
-	      // User is Online but wrong URL and page not found
-	      if(response.status === 404){
+	       //Check if you get the match in the cache - return as such, else fetch from the network
+	       if(response) return response;
 
-	        // Page Not Found - Return Custom Error Message:
-	        return new Response('Whooops! You\'re looking for something that doesn\'t exist here.');
-
-	      }
-
-	      // User is Online, so return the response
-	      return response;
-
-	    }).catch(function(){
-
-	      // User is Offline - Return custom Response
-	      return new Response('You seem to be offline!');
-
+	       //return from Network if no match from Cache
+	       return fetch(event.request);
 	    })
 	);
 });
--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,28 @@
+'use strict';
+
+importScripts('serviceworker-cache-polyfill.js');
+
 // INSTALL EVENT:
 // Called only once, when browser sees this version of Service Worker
 // for the first time
 self.addEventListener('install', function(event){
 	console.info('[SUCCESS] Service Worker Installed');
-	
-	event.waitUntil(
 
+	event.waitUntil(
 		// Opens a new cache
-		caches.open('cache_v1').then(function(cache){
+		caches.open('cache_sw_root_1').then(function(cache){
+			console.info('[SUCCESS] Loading into Cache');
 			return cache.addAll([
 
 				// Load this array into cache
-				'/'
-				
+				'/',
+				'style.css',
+				'img/AD.png',
+				'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+				new Request('https://farm6.staticflickr.com/5594/14749918329_888df4f2ef.jpg')
 			]);
+		}).catch(function(){
+			console.info('[FAILURE] Loading Cache');
 		})
 	);
 });
@@ -27,29 +36,18 @@ self.addEventListener('activate', function(event){
 
 });
 
+
+// FETCH EVENT:
+// Fires up when Service Worker encounters a fetch request
 self.addEventListener('fetch', function(event){
 	console.info('[FETCH] ' + event.request.url);
-	
+
+	// Fetching from Cache:
 	event.respondWith(
-		// Fetch from Network:
-	    fetch(event.request).then(function(response){
+	    caches.match(event.request).then(function(response){
 
-	      // User is Online but wrong URL and page not found
-	      if(response.status === 404){
-
-	        // Page Not Found - Return Custom Error Message:
-	        return new Response('Whooops! You\'re looking for something that doesn\'t exist here.');
-
-	      }
-
-	      // User is Online, so return the response
-	      return response;
-
-	    }).catch(function(){
-
-	      // User is Offline - Return custom Response
-	      return new Response('You seem to be offline!');
-
+	       //Check if you get the match in the cache - return as such, else fetch from the network
+	       return response || fetch(event.request);
 	    })
 	);
 });
